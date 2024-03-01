32f9932649ca516092bb373cc16bfec0b5d5a7e1
Samar Benamar <samar_benamar@ext.carrefour.com>
2024-03-01 21:39:20 +0100

    initial commit

--- transaction-script-pattern/src/main/java/org/example/BankMain.java
+++ transaction-script-pattern/src/main/java/org/example/BankMain.java
@@ -1,14 +1,7 @@
 package org.example;
 
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class BankMain {
     public static void main(String[] args) throws Exception {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
         Account account = new Account(1, 100);
         Transaction transaction = new Transaction(account.getId(), 50, "W");
         BankTransaction bankTransaction = new BankTransaction("jdbc:postgresql://localhost:5432/test?user=postgres&password=postgres");
